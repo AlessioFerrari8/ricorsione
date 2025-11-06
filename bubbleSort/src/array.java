9ce3c9bd838e88f78b6b2c3f83228ef0dba284f9
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-11-06 11:52:13 +0100

    Feat: add shaker Sort

--- bubbleSort/src/array.java
+++ bubbleSort/src/array.java
@@ -1,9 +1,20 @@
+import java.util.Arrays;
+
 public class array {
     public static void main(String[] args) throws Exception {
         int[] array = {2, 5, 11, 17, 12, 48, 456};
         System.out.println(cercaMinimo(array, array[0], array.length - 1));
         int[] array2 = {2, 5, 11, 17, 12, 48, 456};
         System.out.println(cercaMinimo2(array2, array.length - 1));
+
+        int[] es = {5, 4, 1, 3, 9, 12};
+        bubbleSort(es, es.length); 
+        System.out.println(Arrays.toString(es));
+
+        int [] esesmepio = {5, 3, 7, 9, 1, 5, 6};
+        shakerSortLeft(esesmepio, 0, esesmepio.length - 1);        
+        System.out.println(Arrays.toString(esesmepio));
+
 
     }
 
@@ -40,7 +51,9 @@ public class array {
             if (v[i] > v[i + 1])
                 swap(v, i, i + 1);
         }
-        bubbleSort(v, ++fine);
+
+        bubbleSort(v, --fine);
+        return;
     }
 
     public static void swap(int[] v, int i, int j) {
@@ -48,5 +61,35 @@ public class array {
         temp = v[i];
         v[i] = v[j];
         v[j] = temp;
+    }
+
+    // public static void shakerSort(int[] v, int fine) {
+
+    // }
+
+    public static void shakerSortRiht(int[] v, int inizio, int fine) {
+        if(fine <= inizio) {
+            return;
+        }
+
+        for (int i = inizio; i < fine; i++) {
+            if (v[i] > v[i+1])
+                swap(v, i, i + 1);
+        }
+
+        shakerSortLeft(v, inizio, --fine);
+    }
+
+    public static void shakerSortLeft(int[] v, int inizio, int fine) {
+        if(fine <= inizio) {
+            return;
+        }
+
+        for (int i = fine; i > inizio; i--) {
+            if (v[i] < v[i-1])
+                swap(v, i, i - 1);
+        }
+
+        shakerSortRiht(v, ++inizio, fine);
     }
 }
